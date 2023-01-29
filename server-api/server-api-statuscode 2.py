11d979f733f62645190fdbfc55b532483e4ba10f
gung <71138589+agungsoboru@users.noreply.github.com>
2023-01-30 02:30:19 +0800

    Update server-api-statuscode 2.py

--- server-api/server-api-statuscode 2.py
+++ server-api/server-api-statuscode 2.py
@@ -2,6 +2,8 @@
 import random
 from fastapi import FastAPI
 import requests
+import subprocess
+
 app = FastAPI()
 
 @app.get("/status/{domain}/")
@@ -13,3 +15,10 @@ def api():
     phrases = ["API is online and running smoothly","All systems are operational","API is responding normally","API is available and healthy","API connectivity is confirmed","API is up and running","API is accessible and performing well","API is functioning as expected","API is accessible and responding","API is reachable and operational"]
 
     return (random.choice(phrases))
+
+
+@app.get("/tmux")
+def tmux():
+    capture = subprocess.run(["tmux", "capture-pane", "-t", "0"], capture_output=True, text=True)
+    show = subprocess.run(["tmux", "show-buffer"], capture_output=True, text=True)
+    return show.stdout
